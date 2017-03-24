86a5671a8f8f936d6a6325d2bb511e450fdf6f29
Daniel Tea <daniel_tea@spe.sony.com>
2017-03-24 13:14:23 -0700

    basic index.html

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('and my axe!');
+const express = require('express');
+const app = express();
+
+app.listen(8080, function() {
+  console.log('listening on 8080');
+});
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
