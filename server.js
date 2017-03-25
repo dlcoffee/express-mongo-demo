78f525dcc6b47b0271409ecd7a15e2c3f21606e1
Daniel Tea <daniel_tea@spe.sony.com>
2017-03-24 23:32:30 -0700

    connect to mongodb

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
 const app = express();
 
 // middleware
@@ -7,18 +9,44 @@ app.use(bodyParser.urlencoded({
   extended: true,
 }));
 
+// mongoDB setup
+var db;
+MongoClient.connect(
+  'mongodb://' +
+  dotenv.parsed.DB_USER + ':' + dotenv.parsed.DB_PASSWORD +
+  '@ds141410.mlab.com:41410/lotr-quotes',
+  (err, database) => {
+    if (err) {
+      return console.log(err);
+    }
 
-app.listen(8080, function() {
-  console.log('listening on 8080');
-});
+    db = database;
+    app.listen(8080, function() {
+      console.log('listening on 8080');
+    });
+  }
+);
 
 
 // GETs
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
+  var cursor = db.collection('quotes')
+    .find()
+    .toArray((err, results) => {
+      console.log(results);
+    });
 });
 
 // POSTs
 app.post('/quotes', (req, res) => {
-  console.log(req.body);
+  db.collection('quotes')
+    .save(req.body, (err, result) => {
+      if (err) {
+        return console.log(err);
+      }
+
+      console.log('saved to the database');
+      res.redirect('/');
+    });
 });
